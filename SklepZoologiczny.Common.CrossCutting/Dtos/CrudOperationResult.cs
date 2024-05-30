b598d091133abe62e4d8ef88349bd04b35f3e60e
Rafał Wątroba <rafalwatroba@protonmail.com>
2024-05-30 09:34:03 +0200

    Init Project

---
+++ SklepZoologiczny.Common.CrossCutting/Dtos/CrudOperationResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SklepZoologiczny.Common.CrossCutting.Enums;
+
+namespace SklepZoologiczny.Common.CrossCutting.Dtos
+{
+    public class CrudOperationResult<TDto>
+    {
+        public CrudOperationResultStatus Status { get; set; }
+
+        public TDto? Result { get; set; }
+    }
+}
---
+++ SklepZoologiczny.Common.CrossCutting/Enums/CrudOperationResultStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SklepZoologiczny.Common.CrossCutting.Enums
+{
+    public enum CrudOperationResultStatus
+    {
+        Success,
+        Failure,
+        RecordNotFound
+    }
+}
