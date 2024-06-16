52235234c03ceba826f4c3c929727d271549bfda
Nokijoto <nokijoto@protonmail.com>
2024-06-16 12:53:44 +0200

    Update Seeder

--- SklepZoologiczny.Warehouse/Seeder.cs
+++ SklepZoologiczny.Warehouse/Seeder.cs
@@ -39,21 +39,40 @@ namespace SklepZoologiczny.Warehouse
 
         private IEnumerable<Product> GetProducts()
         {
-            var categoryId = _context.Categories.FirstOrDefault()?.Id;
-            var supplierId = _context.Suppliers.FirstOrDefault()?.Id;
+            var categories = _context.Categories.ToList();
+            var suppliers = _context.Suppliers.ToList();
 
             return new List<Product>()
         {
             new Product()
             {
                 Id = Guid.NewGuid(),
-                Name = "TestProduct",
-                Description = "TestDescription",
-                Price = 100,
-                Quantity = 10,
-                CategorieId = categoryId ?? Guid.Empty,
-                SupplierId = supplierId ?? Guid.Empty,
-                
+                Name = "Karma dla psów",
+                Description = "Wysokiej jakości karma dla psów",
+                Price = 50,
+                Quantity = 100,
+                CategorieId = categories[0].Id,
+                SupplierId = suppliers[0].Id
+            },
+            new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Zabawka dla kota",
+                Description = "Interaktywna zabawka dla kotów",
+                Price = 20,
+                Quantity = 200,
+                CategorieId = categories[1].Id,
+                SupplierId = suppliers[1].Id
+            },
+            new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Klatka dla ptaków",
+                Description = "Przestronna i bezpieczna klatka dla ptaków",
+                Price = 150,
+                Quantity = 50,
+                CategorieId = categories[2].Id,
+                SupplierId = suppliers[2].Id
             }
         };
         }
@@ -65,11 +84,26 @@ namespace SklepZoologiczny.Warehouse
             new Categorie()
             {
                 Id = Guid.NewGuid(),
-                Name = "TestCategory",
-                Description = "TestCategoryDescription",
+                Name = "Karma dla zwierząt",
+                Description = "Wszystkie rodzaje karmy dla zwierząt",
                 ParentCategory = null,
-                ParentCategoryId = null,
-               
+                ParentCategoryId = null
+            },
+            new Categorie()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Zabawki dla zwierząt",
+                Description = "Różne zabawki dla zwierząt",
+                ParentCategory = null,
+                ParentCategoryId = null
+            },
+            new Categorie()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Akcesoria dla zwierząt",
+                Description = "Akcesoria dla zwierząt",
+                ParentCategory = null,
+                ParentCategoryId = null
             }
         };
         }
@@ -82,14 +116,37 @@ namespace SklepZoologiczny.Warehouse
             {
                 Id = Guid.NewGuid(),
                 City = "Warszawa",
-                Name = "Supplier1",
-                Country = "Poland",
-                Street = "Testowa 1",
-                Email = "supplier1@example.com",
+                Name = "Zwierzakowo",
+                Country = "Polska",
+                Street = "Krakowska 1",
+                Email = "kontakt@zwierzakowo.pl",
                 Phone = "123456789",
                 State = "Mazowieckie",
-                ZipCode = "00-000",
-                
+                ZipCode = "00-001"
+            },
+            new Supplier()
+            {
+                Id = Guid.NewGuid(),
+                City = "Kraków",
+                Name = "Planeta Zwierząt",
+                Country = "Polska",
+                Street = "Floriańska 5",
+                Email = "info@planetazwierzat.pl",
+                Phone = "987654321",
+                State = "Małopolskie",
+                ZipCode = "30-002"
+            },
+            new Supplier()
+            {
+                Id = Guid.NewGuid(),
+                City = "Gdańsk",
+                Name = "Supplies dla Zwierząt",
+                Country = "Polska",
+                Street = "Długa 10",
+                Email = "support@supplies.pl",
+                Phone = "555555555",
+                State = "Pomorskie",
+                ZipCode = "80-003"
             }
         };
         }
